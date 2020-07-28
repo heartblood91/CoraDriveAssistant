c3e49106079a6573bca0f63aa5f873ac36f24dac
Cédric <github@hidemyhome.ovh>
2020-07-28 16:40:10 +0200

    AJOUT: fonction sous forme de promesse permettant de mettre à jour le panier (ajout / suppression d'item) selon l'ingredient transmis par Google Assistant-IFTTT (après extraction des mots clés)

--- route.js
+++ route.js
@@ -1,16 +1,9 @@
-const updateProduct = require("./updateProduct");
 const authentification = require("./authentfication");
 const check = require("./checkPanier");
 const setListProduct = require("./setListProduct");
 
 // Gestion des routes du serveur backend
 module.exports = function (expressServer) {
-  // Récupère des informations provenant de IFTT
-
-  // MAJ des produits (ajout / suppression)
-  expressServer.post("/add/:product", updateProduct.add);
-  expressServer.post("/delete/:product", updateProduct.delete);
-
   //Authentification
   expressServer.post("/login", authentification.login);
 
--- updateProduct.js
+++ updateProduct.js
@@ -3,124 +3,123 @@ const axios = require("axios");
 // Module contenant les credentials
 credentialsModule = require("./credentials");
 
-// Ajoute un élément à la liste de course
-module.exports.add = function (req, res, next) {
-  // Récupère le produit envoyer en paramètre
-  const product = req.params.product;
+// Récupère la BDD (fichier json)
+const jsonBDD = require("./constante/list-product.json");
 
-  //Création de la requête pour ajouter un produit:
-  const config = createRequest("add");
+//Fonction, sous forme de promesse, qui ajoute ou suprime un élément à la liste de course
+module.exports.update = (addOrRemove, product) => {
+  return new Promise((resolve, reject) => {
+    //Création de la requête pour ajouter un produit:
+    const config = createRequest(addOrRemove, product);
 
-  axios(config)
-    .then(function (response) {
-      return res.status(200).send(response.data);
-    })
-    .catch(function (error) {
-      return res.status(401).send(error);
-    });
+    // Si la fonction nous retourne une non-correspondance alors on rejete directement la promesse
+    // Si correspondance, alors on envoie la requête via Axios
+    config === "Produit inconnu" && reject(config);
+
+    axios(config)
+      .then(function (response) {
+        resolve(response.data);
+      })
+      .catch(function (err) {
+        reject(err);
+      });
+  });
 };
 
-// Supprime un élément à la liste de course
-module.exports.delete = function (req, res, next) {
-  // Récupère le produit envoyer en paramètre
-  const product = req.params.product;
+const createRequest = (type, nameOfProduct) => {
+  // Etape 1: Je recherche une correspondance entre le produit transmis par le client et la BDD (+ je récupère les caractéristiques de ce produit dans la BDD)
+  let theProduct;
 
-  //Création de la requête pour ajouter un produit:
-  const config = createRequest("delete");
+  jsonBDD.map((productBDD) => {
+    if (productBDD.googleIngredientCmd.trim().toLowerCase() === nameOfProduct) {
+      // Copie l'ensemble des caractéristiques du produit
+      theProduct = Object.assign({}, productBDD);
 
-  axios(config)
-    .then(function (response) {
-      return res.status(200).send(response.data);
-    })
-    .catch(function (error) {
-      return res.status(401).send(error);
-    });
-};
-
-const createRequest = (type) => {
-  // Produit factice qui devrait provenir à terme d'une 'BDD'
-  let productToTest = {
-    idProduct: "2983",
-    designation: "Cora comté AOP  au lait cru 5 mois d'affinage minimum 200g",
-    pft: 0,
-    quantite: 0,
-    prix: "2.37",
-    context_id: 244,
-    syncID: null,
-  };
-
-  // Construction de data (body)
-  // Etape 1: Je vérifie l'existance du produit dans le panier
-  let productIsPresent = false;
-  let indexOfProduct = 0;
-
-  // Permet de vérifier si le produit est présent pour un update
-  credentialsModule.shopCart.itemsPanier.map((item, index) => {
-    if (item.idProduct === productToTest.idProduct) {
-      productIsPresent = true;
-      indexOfProduct = index;
+      // Supprime les informations innutiles
+      delete theProduct.googleIngredientCmd;
     }
   });
 
-  // Etape 1bis: si le produit est présent, je mets à jour le produit selon les informations que je dispose sinon je conserve les informations provenant du backend
-  let newProduct = productIsPresent
-    ? credentialsModule.shopCart.itemsPanier[indexOfProduct]
-    : productToTest;
+  // Je vérifie qu'un produit a été trouvé dans la BDD
+  // Si aucune correspondance, je stop le script et je fais un retour à l'utilisateur
 
-  // Etape2: En fonction du type (achat VS vente) je mets à jour la quantité
-  if (type === "add") {
-    newProduct.quantite = newProduct.quantite + 1;
-  } else if (type === "delete") {
-    newProduct.quantite = 0;
+  if (theProduct === undefined) {
+    return "Produit inconnu";
+  } else {
+    // Construction de data (body)
+    // Etape 2: Je vérifie l'existance du produit dans le panier
+    let productIsPresent = false;
+    let indexOfProduct = 0;
+
+    // Permet de vérifier si le produit est présent pour un update
+    credentialsModule.shopCart.itemsPanier.map((item, index) => {
+      if (item.idProduct === theProduct.idProduct) {
+        productIsPresent = true;
+        indexOfProduct = index;
+      }
+    });
+
+    // Etape 3: si le produit est présent, je mets à jour le produit selon les informations que je dispose sinon je conserve les informations provenant du backend
+    let newProduct = productIsPresent
+      ? Object.assign(
+          {},
+          credentialsModule.shopCart.itemsPanier[indexOfProduct]
+        )
+      : Object.assign({}, theProduct);
+
+    // Etape 4: En fonction du type (achat VS vente) je mets à jour la quantité
+    newProduct.quantite = type === "add" ? newProduct.quantite + 1 : 0;
+
+    // Etape 5: création du body
+    const data =
+      '{"items":[{"id":' +
+      newProduct.idProduct +
+      ',"designation":"' +
+      newProduct.designation +
+      '","pft":' +
+      newProduct.pft +
+      ',"quantite":' +
+      newProduct.quantite +
+      ',"prix":"' +
+      newProduct.prix +
+      '","context_id":' +
+      newProduct.context_id +
+      ', "syncId":' +
+      newProduct.syncID +
+      "}]}";
+
+    //Etape 6: Création de la config
+    const config = {
+      method: "post",
+      url:
+        "https://api.coradrive.fr/api/magasins/120/panier/" +
+        credentialsModule.shopCart.idPanier +
+        "/items?uuid=" +
+        credentialsModule.uuidCora +
+        "&" +
+        credentialsModule.shopCart.lastSync,
+      headers: {
+        Host: "api.coradrive.fr",
+        Pragma: "no-cache",
+        Authorization: "Bearer " + credentialsModule.token,
+        "cora-auth": "apidrive",
+        Accept: "application/vnd.api.v1+json",
+        "Cache-Control": "no-cache",
+        "app-id": "1",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+        "Content-Type": "application/json;charset=UTF-8",
+        "app-signature":
+          "BROWSER;WEB;81.0.4044.138;;2;1;2.5.17;Chrome;900;1440",
+        Origin: "https://www.coradrive.fr",
+        "Sec-Fetch-Site": "same-site",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Accept-Language": "fr,en-US;q=0.9,en;q=0.8",
+      },
+      data: data,
+    };
+
+    return config;
   }
-
-  // Etape3: création du body
-  const data =
-    '{"items":[{"id":' +
-    newProduct.idProduct +
-    ',"designation":"' +
-    newProduct.designation +
-    '","pft":' +
-    newProduct.pft +
-    ',"quantite":' +
-    newProduct.quantite +
-    ',"prix":"' +
-    newProduct.prix +
-    '","context_id":' +
-    newProduct.context_id +
-    ', "syncId":' +
-    newProduct.syncID +
-    "}]}";
-
-  const config = {
-    method: "post",
-    url:
-      "https://api.coradrive.fr/api/magasins/120/panier/" +
-      credentialsModule.shopCart.idPanier +
-      "/items?uuid=" +
-      credentialsModule.uuidCora +
-      "&" +
-      credentialsModule.shopCart.lastSync,
-    headers: {
-      Host: "api.coradrive.fr",
-      Pragma: "no-cache",
-      Authorization: "Bearer " + credentialsModule.token,
-      "cora-auth": "apidrive",
-      Accept: "application/vnd.api.v1+json",
-      "Cache-Control": "no-cache",
-      "app-id": "1",
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
-      "Content-Type": "application/json;charset=UTF-8",
-      "app-signature": "BROWSER;WEB;81.0.4044.138;;2;1;2.5.17;Chrome;900;1440",
-      Origin: "https://www.coradrive.fr",
-      "Sec-Fetch-Site": "same-site",
-      "Sec-Fetch-Mode": "cors",
-      "Sec-Fetch-Dest": "empty",
-      "Accept-Language": "fr,en-US;q=0.9,en;q=0.8",
-    },
-    data: data,
-  };
-
-  return config;
 };
