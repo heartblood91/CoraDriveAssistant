1832bdc5730a0a6f6e4affe844e2653f453a1ddf
Cédric <github@hidemyhome.ovh>
2020-08-14 16:23:21 +0200

    Ajout: image du produit visible dans le mail pour simplifier la lecture

--- sequenceSendCartByMail.js
+++ sequenceSendCartByMail.js
@@ -4,6 +4,7 @@ const checkCart = require("./checkPanier");
 const verificator = require("./verify");
 const notification = require("./notif");
 const senderMail = require("./sendMail");
+const configFile = require("./constante/config");
 
 // Fonction permettant de formater un panier en une belle liste de course pour un envoie par mail
 const formatMyCartToList = () => {
@@ -15,13 +16,15 @@ const formatMyCartToList = () => {
   bodyMail.withoutHTML =
     "Bonjour, \nVeuillez trouver ci-dessous votre liste de course: \n\n";
   bodyMail.withHTML =
-    "Bonjour, <br>Veuillez trouver ci-dessous votre liste de course: <br><ul>";
+    '<html> <head> <meta http-equiv="content-type" content="text/html; charset=UTF-8"> </head> <body> Bonjour, <br>Veuillez trouver ci-dessous votre liste de course: <br><ul>';
 
   configFile.shopCart.itemsPanier.map((item) => {
     // Je prends l'ancienne valeur du total et j'aoute le produit (prix unitaire * sa quantité)
     totalPrice = totalPrice + parseFloat(item.prix) * parseInt(item.quantite);
 
     // Je prends le contenu de mail et j'ajoute le nouveau produit (selon HTML/Text simple)
+
+    // Version texte simple:
     bodyMail.withoutHTML =
       bodyMail.withoutHTML +
       "-" +
@@ -29,13 +32,21 @@ const formatMyCartToList = () => {
       " X " +
       item.quantite +
       "\n";
+
+    // En version HTML:
+
     bodyMail.withHTML =
       bodyMail.withHTML +
       "<li>" +
+      '<img data-v-308641df="" src="' +
+      item.visuel +
+      '" alt="' +
       item.designation +
-      " X " +
+      '" class="photo" moz-do-not-send="true" width="50" height="50"></img> X ' +
       item.quantite +
-      "</li>";
+      " (<i>" +
+      item.designation +
+      "</i>)</li>";
 
     item.designation + " X " + item.quantite;
   });
@@ -50,8 +61,8 @@ const formatMyCartToList = () => {
     bodyMail.withHTML +
     "<br><li><b>PRIX TOTAL: " +
     totalPrice.toFixed(2) +
-    "</b></li>" +
-    "</ul><br>Amicalement,<br>Votre assistante";
+    "€</b></li>" +
+    "</ul><br>Amicalement,<br>Votre assistante   </body> </html>";
 
   // Je renvoie la variable
   return bodyMail;
@@ -75,6 +86,7 @@ module.exports.sendMeCartByMail = function (req, res, next) {
                 if (resultat === "Ok") {
                   // Je transforme mon panier en une liste
                   const bodyMail = formatMyCartToList();
+                  // res.status(200).send("ok");
 
                   senderMail
                     .sendMeAMail(bodyMail)
